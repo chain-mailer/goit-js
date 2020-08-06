c68984826e9950f02a16da9edcfae3d00d697143
Viktor Petliuk <pajalnik.st.gorod@gmail.com>
2020-08-06 20:12:29 +0300

    fixed hw-04

--- goit-js-hw-04/js/task-02.js
+++ goit-js-hw-04/js/task-02.js
@@ -52,7 +52,7 @@ const inventory = {
 
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action(itemName);
+  action.call(inventory, itemName);
 };
 
 invokeInventoryAction('Medkit', inventory.add);
