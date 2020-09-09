155e62eae0e9574220c43e45db340e5e0d5e47f9
Viktor Petliuk <pajalnik.st.gorod@gmail.com>
2020-09-09 12:14:15 +0300

    added hw-07

--- goit-js-hw-07/js/task-04.js
+++ goit-js-hw-07/js/task-04.js
@@ -2,30 +2,31 @@ const decrementBtn = document.querySelector('.decrement-js');
 const incrementBtn = document.querySelector('.increment-js');
 const value = document.querySelector('#value');
 
-const subtraction = (a, b) => a - b;
-const add = (a, b) => a + b;
-
-decrementBtn.addEventListener('click', () => {
-  value.textContent = subtraction(+value.textContent, 1);
-});
-
-incrementBtn.addEventListener('click', () => {
-  value.textContent = add(+value.textContent, 1);
-});
-
-// ---------------------------
-const valueRef = document.querySelector('#value');
-const incrementBtnRef = document.querySelector('button[data-action="increment"]');
-const decrementBtnRef = document.querySelector('button[data-action="decrement"]');
-
-let counterValue = +valueRef.textContent;
-
-function increment() {
-  valueRef.textContent = counterValue += 1;
+function subtraction() {
+  --value.textContent;
 }
-function decrement() {
-  valueRef.textContent = counterValue -= 1;
+function add() {
+  ++value.textContent;
 }
 
-incrementBtnRef.addEventListener('click', increment);
-decrementBtnRef.addEventListener('click', decrement);
+decrementBtn.addEventListener('click', subtraction);
+incrementBtn.addEventListener('click', add);
+
+// // ---------------------------
+// 2-й вариант
+
+// const valueRef = document.querySelector('#value');
+// const incrementBtnRef = document.querySelector('button[data-action="increment"]');
+// const decrementBtnRef = document.querySelector('button[data-action="decrement"]');
+
+// let counterValue = +valueRef.textContent;
+
+// function increment() {
+//   valueRef.textContent = counterValue += 1;
+// }
+// function decrement() {
+//   valueRef.textContent = counterValue -= 1;
+// }
+
+// incrementBtnRef.addEventListener('click', increment);
+// decrementBtnRef.addEventListener('click', decrement);
